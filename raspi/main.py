3aad6ba14cf1b4664496eebfa5ad5e30eb5e1ab0
Jannis Schröter <73310649+RaveX07@users.noreply.github.com>
2025-04-27 13:25:30 +0200

    Prepare for xy test

--- raspi/main.py
+++ raspi/main.py
@@ -29,7 +29,7 @@ class RobotController:
 
         self.camera = Camera() if CAM else None
         
-        self.task = Task(motor_controller=self.motor_controller, action_set=[['tt90']])
+        self.task = Task(motor_controller=self.motor_controller, action_set=[['d500']])
         
     def add_task(self, actions: list[str]):
         task = Task(actions=actions)
