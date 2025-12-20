8e4ff0a957f8fa57ecccbbdfdf1a296897d92b36
BlackDemon1000 <cb1.10.10.10@outlook.de>
2025-12-20 17:04:06 +0100

    gegi code

--- First Test/src/main.cpp
+++ First Test/src/main.cpp
@@ -3,8 +3,17 @@
 #define ECHO_1 3
 #define TRIG_2 4
 #define ECHO_2 5
+#define TRIG_3 6
+#define ECHO_3 7
+#define TRIG_4 8
+#define ECHO_4 9
+#define TRIG_5 10
+#define ECHO_5 11
+#define TRIG_6 12
+#define ECHO_6 13
 
-const int GEGNER_DIST = 30; // cm
+const int GEGNER_DIST = 15; // cm
+bool ALARM = false;
 
 long messen(int trig, int echo) {
   digitalWrite(trig, LOW);
@@ -24,10 +33,24 @@ void setup() {
   pinMode(ECHO_1, INPUT);
   pinMode(TRIG_2, OUTPUT);
   pinMode(ECHO_2, INPUT);
-
+  pinMode(TRIG_3, OUTPUT);
+  pinMode(ECHO_3, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(messen(TRIG_1, ECHO_1));
+  if (messen(TRIG_1,ECHO_1) <= GEGNER_DIST || messen(TRIG_2,ECHO_2) <= GEGNER_DIST || messen(TRIG_3,ECHO_3) <= GEGNER_DIST || messen(TRIG_4,ECHO_4) <= GEGNER_DIST || messen(TRIG_5,ECHO_5) <= GEGNER_DIST || messen(TRIG_6,ECHO_6) <= GEGNER_DIST)
+  {
+    ALARM = true;
+  }
+  else{
+    ALARM = false;
+  }
+  if (ALARM)
+  {
+    Serial.println("Gegner erkannt");
+  }
+  else{
+    Serial.println("kein Gegner erkannt");
+  }
 }
