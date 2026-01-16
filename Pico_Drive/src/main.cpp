76ea5ce78a9c36ee1d8a4717306e4e534744a4d8
BlackDemon1000 <cb1.10.10.10@outlook.de>
2026-01-16 10:32:35 +0100

    drive code

--- Pico_Drive/src/main.cpp
+++ Pico_Drive/src/main.cpp
@@ -1,11 +1,58 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
 
+#define L_STEP 1
+#define L_DIR 2
+#define R_STEP 3
+#define R_DIR 4
+#define STOP_PIN 5
+
+AccelStepper stepperLeft(AccelStepper::DRIVER, L_STEP, L_DIR);
+AccelStepper stepperRight(AccelStepper::DRIVER, R_STEP, R_DIR);
+
+//Interrupt Flag
+volatile bool emergencyStop = false;
+
+void stopISR() {
+  emergencyStop = true;
+}
+
+void setSpeed(float left, float right) {
+  stepperLeft.setSpeed(left);
+  stepperRight.setSpeed(right);
+}
+
+
+void geradeaus(float speed) {
+  setSpeed(speed, speed);
+}
+
+
+void drehen(float speed) {
+  setSpeed(speed, -speed);
+}
+
+void stopMotoren() {
+  stepperLeft.setSpeed(0);
+  stepperRight.setSpeed(0);
+}
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(STOP_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(STOP_PIN), stopISR, FALLING);
+
+  stepperLeft.setMaxSpeed(2000);
+  stepperRight.setMaxSpeed(2000);
+
+  stepperLeft.setAcceleration(1000);
+  stepperRight.setAcceleration(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (emergencyStop) {
+    stopMotoren();
+    return;
+  }
+  stepperLeft.runSpeed();
+  stepperRight.runSpeed();
 }
-
