9c246f78f43053787ba4e270e5b6ea1c79686f94
BlackDemon1000 <gw1_10@gmx.de>
2026-03-10 23:32:44 +0100

    working

--- ESP/src/main.cpp
+++ ESP/src/main.cpp
@@ -1,15 +1,28 @@
 #include <Arduino.h>
+#define STEP1 25
+#define DIR1  26
 
-#define LED_PIN 2
+#define STEP2 32
+#define DIR2  33
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(STEP1, OUTPUT);
+  pinMode(DIR1, OUTPUT);
+  pinMode(STEP2, OUTPUT);
+  pinMode(DIR2, OUTPUT);
+
+  digitalWrite(DIR1, HIGH);
+  digitalWrite(DIR2, HIGH);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH); // LED an
-  delay(500);
 
-  digitalWrite(LED_PIN, LOW);  // LED aus
-  delay(500);
+  digitalWrite(STEP1, HIGH);
+  digitalWrite(STEP2, HIGH);
+  delayMicroseconds(500);
+
+  digitalWrite(STEP1, LOW);
+  digitalWrite(STEP2, LOW);
+  delayMicroseconds(500);
+
 }
