59307a46b0127540d9c2bae74ecbfbe4f3c73cfc
BlackDemon1000 <cb1.10.10.10@outlook.de>
2026-04-13 09:21:27 +0100

    cleanup

--- raspi/modules/drive_state.py
+++
@@ -1,9 +0,0 @@
-class DriveState():
-    def __init__(self, x: int, y: int, theta: float, finished: bool, task, stopped: bool, direction: int = 1):
-        self.x = x
-        self.y = y
-        self.theta = theta
-        self.finished = finished
-        self.task = task
-        self.stopped = stopped
-        self.direction = direction  # 1: forward, 0: turn, -1: backwards
